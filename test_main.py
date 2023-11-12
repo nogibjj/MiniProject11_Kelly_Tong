d5e874bc99d0a93e01b943c50b2d7f9a8a83513a
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-11-12 15:22:35 -0500

    Initial commit

---
+++ test_main.py
@@ -0,0 +1,56 @@
+"""
+Test goes here
+
+"""
+
+import subprocess
+
+
+def test_extract():
+    """tests extract()"""
+    result = subprocess.run(
+        ["python", "main.py", "extract"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+    assert "Extracting data..." in result.stdout
+
+
+def test_transform_load():
+    """tests transfrom_load"""
+    result = subprocess.run(
+        ["python", "main.py", "transform_load"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+    assert "Transforming data..." in result.stdout
+
+
+def test_general_query():
+    """tests general_query"""
+    result = subprocess.run(
+        [
+            "python",
+            "main.py",
+            "general_query",
+            """SELECT Major, SUM(Total) as SumTotal, SUM(Women) as SumWomen 
+            FROM women_stemDB 
+            GROUP BY Major 
+            ORDER BY SumTotal DESC 
+            LIMIT 10""",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert result.returncode == 0
+
+
+if __name__ == "__main__":
+    test_extract()
+    test_transform_load()
+    test_general_query()
